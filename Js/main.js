587d9b9579c544846e07f94233b97248d785a981
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-02 14:18:07 +0200

    change outputMessage

--- Js/main.js
+++ Js/main.js
@@ -2,18 +2,19 @@
 const multipleOfThree = "Fizz";
 const multipleOfFive = "Buzz";
 const multipleOfThreeAndFive = "FizzBuzz";
+const myWindows = document.open();
 let outputMessage;
 
 // Execute Script
 for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
-    outputMessage = multipleOfThreeAndFive;
+    outputMessage = `${i} : "${multipleOfThreeAndFive}" (Multiplo di 3 e di 5)`;
   } else if (i % 3 == 0) {
-    outputMessage = multipleOfThree;
+    outputMessage = `${i} : "${multipleOfThree}" (Multiplo di 3)`;
   } else if (i % 5 == 0) {
-    outputMessage = multipleOfFive;
+    outputMessage = `${i} : "${multipleOfFive}" (Multiplo di 5)`;
   } else {
     outputMessage = i;
   }
-  console.log(outputMessage);
+  console.log(`${outputMessage}`);
 }
