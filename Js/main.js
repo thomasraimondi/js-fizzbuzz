a0a1ef9677f376cdf062ccd9a2c4cefd8cecdf45
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-02 13:50:20 +0200

    print Fizz for numebr multiple of 3

--- Js/main.js
+++ Js/main.js
@@ -5,6 +5,8 @@ const multipleOfThreeAndFive = "FizzBuzz";
 for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
     console.log(multipleOfThreeAndFive);
+  } else if (i % 3 == 0) {
+    console.log(multipleOfThree);
   } else {
     console.log(i);
   }
