3036a6b71aebd41b2ddd58ca3e4e193b02cf9c27
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-02 13:48:55 +0200

    print number multiple 3 and 5

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+const multipleOfThree = "Fizz";
+const multipleOfFive = "Buzz";
+const multipleOfThreeAndFive = "FizzBuzz";
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(multipleOfThreeAndFive);
+  } else {
+    console.log(i);
+  }
+}
